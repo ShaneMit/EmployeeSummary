a83a7fd7eb051023c38a1172d0695b41d5114588
ShaneMit <shanemit@cox.net>
2020-05-18 21:53:57 -0700

    added constructors and started running tests

---
+++ app.js
@@ -0,0 +1,9 @@
+// Class Constructors
+// Must be a class
+// Must have constructor
+// Must take in name, email, and id
+// must have functions that retrieve name, email, id, and role (string value)
+
+// Employee 
+
+
